5bbc96b0c254d46dc2c2f9ddd909444cfe82f3bf
mccarrmb <matt.mccarragher@online-rewards.com>
2018-07-17 09:15:00 -0400

    fixed directory

--- google-automator.rb
+++ google-automator.rb
@@ -9,7 +9,7 @@ class GoogleTest < Minitest::Test
   log_directory = File.join(File.dirname(__FILE__), "log")
   Dir.mkdir(log_directory) unless File.exists?(log_directory)
   Selenium::WebDriver.logger.level = :debug
-  Selenium::WebDriver.logger.output = File.join(File.dirname(__FILE__), "..", "log", "selenium.log")
+  Selenium::WebDriver.logger.output = File.join(File.dirname(__FILE__), "log", "selenium.log")
 
   def setup
     @browser = Selenium::WebDriver.for :firefox
