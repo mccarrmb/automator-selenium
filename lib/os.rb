31257c6837e185a7501b2255bc813ecc07db98ec
Whitey McCarragher <matt.mccarragher@online-rewards.com>
2018-07-18 08:28:11 -0400

    fixed module methods

--- lib/os.rb
+++ lib/os.rb
@@ -1,20 +1,13 @@
 module OS 
-  # Turns out that there isn't a standard way to determine the OS in Ruby
-  
-  def is_macos?
-    if ((/darwin/ =~ RUBY_PLATFORM) == nil)
-      return false
-    else
-      return true
-    end
+  # Turns out that there isn't a standard/safe way to 
+  # determine the OS in Ruby, so this will have to do.
+
+  def self.is_macos?
+    ((/darwin/ =~ RUBY_PLATFORM) == nil) ? false : true
   end
 
-  def is_linux?
-    if ((/linux/ =~ RUBY_PLATFORM) == nil)
-      return false
-    else
-      return true
-    end
+  def self.is_linux?
+    ((/linux/ =~ RUBY_PLATFORM) == nil) ? false : true
   end
 
 end
