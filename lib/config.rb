89c4e307755c64a74a21c650dd909682504d9ce5
Whitey McCarragher <matt.mccarragher@online-rewards.com>
2018-07-27 12:19:16 -0400

    added browser exception

--- lib/config.rb
+++ lib/config.rb
@@ -1,6 +1,14 @@
 require 'selenium-webdriver'
+require_relative 'os.rb'
 
 module Config
+  # Custom exception for bad browser configs
+  class UnknownBrowserError < StandardError
+    attr_reader :object
+    def initialize(object)
+      @object = object
+    end
+  end
   # Class used for setting webdriver options
   class Driver
     def initialize
@@ -10,20 +18,20 @@ module Config
           chrome: File.join(__dir__, '..', 'bin', 'chrome', \
                             'chromedriver_linux64', 'chromedriver'),
           firefox: File.join(__dir__, '..', 'bin', 'firefox', \
-                            'geckodriver-v0.20.1-linux64', 'geckodriver')
+                             'geckodriver-v0.20.1-linux64', 'geckodriver')
         },
         macos: {
           chrome: File.join(__dir__, '..', 'bin', 'chrome', \
                             'chromedriver_mac64', 'chromedriver'),
           firefox: File.join(__dir__, '..', 'bin', 'firefox', \
-                            'geckodriver-v0.20.1-macos', 'geckodriver'),
+                             'geckodriver-v0.20.1-macos', 'geckodriver'),
           safari: File.join('/', 'usr', 'bin', 'safaridriver')
         },
         windows: {
           chrome: File.join(__dir__, '..', 'bin', 'chrome', \
                             'chromedriver_win32', 'chromedriver.exe'),
           firefox: File.join(__dir__, '..', 'bin', 'firefox', \
-                            'geckodriver-v0.20.1-win64', 'geckodriver.exe'),
+                             'geckodriver-v0.20.1-win64', 'geckodriver.exe'),
           edge: File.join(__dir__, '..', 'bin', 'edge', \
                           'microsoft_webdriver_win64', \
                           'MicrosoftWebDriver.exe'),
@@ -40,10 +48,10 @@ module Config
       end
       # Set up Selenium logs
       log_directory = File.join(__dir__, '..', 'log')
-      Dir.mkdir(log_directory) unless File.exist?(log_directory) 
+      Dir.mkdir(log_directory) unless File.exist?(log_directory)
       Selenium::WebDriver.logger.level = :debug
       Selenium::WebDriver.logger.output = File.join(__dir__, '..', 'log', \
-        'selenium.log')
+                                                    'selenium.log')
       # Set driver paths to repo copies
       Selenium::WebDriver::Firefox.driver_path  = drivers[:macos][:firefox]
       Selenium::WebDriver::Chrome.driver_path   = drivers[:macos][:chrome]
@@ -54,33 +62,25 @@ module Config
   end
 
   class App
-    @browsers = [:chrome, 
-                 :firefox,
-                 :edge,
-                 :safari,
-                 :internet_explorer,
-                 :remote,
-                 :html_unit]
-    
+    @browsers = %i[chrome
+                   firefox
+                   edge
+                   safari
+                   internet_explorer
+                   remote
+                   html_unit]
+
     attr_reader :browser
     attr_reader :app_url
-  
 
-    def initialize
-      app_protocol = 'http'
-      app_uri = 'google.com'
-      @browser = @browsers[:firefox]
-      @app_url = app_protocol + '://' + app_uri
-    end
-
-    def initialize(url, protocol = 'http', browser = :firefox)
-    end
-
-    def self.browser
-      @browser
-    end
-
-    def self.app_url
+    def initialize(url = 'google.com', protocol = 'https', browser = :firefox)
+      @app_url = protocol + '://' + url
+      if @browsers.include?(browser)
+        @browser = browser
+      else
+        raise UnknownBrowserError
+          .new(self.browser, 'Browser specified is not supported')
+      end
     end
   end
 end
