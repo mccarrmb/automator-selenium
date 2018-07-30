9588cd57bf37513776d8cb36dce4b22a1cf83e89
Whitey McCarragher <matt.mccarragher@online-rewards.com>
2018-07-30 15:03:52 -0400

    refactoring config

--- google-automator.rb
+++ google-automator.rb
@@ -3,13 +3,13 @@ require 'page-object'
 require 'page-object/page_factory'
 require 'minitest/autorun'
 require 'minitest/unit'
-require_relative 'lib/os.rb'
 require_relative 'lib/config.rb'
 
 # Main test class for setting up config and creating drivers
 class GoogleTest < Minitest::Test
   def setup
-    @browser = Config.App.new(Config::Driver.new(:firefox), 'google.com', 'https')
+    config = Config::Driver.new(:firefox)
+    @browser = Config.App.new(config, 'google.com', 'https')
   end
 
   def teardown
--- lib/config.rb
+++ lib/config.rb
@@ -1,5 +1,6 @@
 require 'selenium-webdriver'
 require 'os'
+require 'pry'
 
 module Config
   # Custom exception for bad browser symbols
@@ -25,63 +26,41 @@ module Config
   end
   # Class used for setting webdriver options
   class Driver
+    attr_reader :browser 
     attr_reader :drivers
-    def initialize(browser)
-      # Stores all relative paths for the repo's drivers
-      drivers = {
-        linux: {
-          chrome: File.join(__dir__, '..', 'bin', 'chrome', \
-                            'chromedriver_linux64', 'chromedriver'),
-          firefox: File.join(__dir__, '..', 'bin', 'firefox', \
-                             'geckodriver-v0.20.1-linux64', 'geckodriver')
-        },
-        macos: {
-          chrome: File.join(__dir__, '..', 'bin', 'chrome', \
-                            'chromedriver_mac64', 'chromedriver'),
-          firefox: File.join(__dir__, '..', 'bin', 'firefox', \
-                             'geckodriver-v0.20.1-macos', 'geckodriver'),
-          safari: File.join('/', 'usr', 'bin', 'safaridriver')
-        },
-        windows: {
-          chrome: File.join(__dir__, '..', 'bin', 'chrome', \
-                            'chromedriver_win32', 'chromedriver.exe'),
-          firefox: File.join(__dir__, '..', 'bin', 'firefox', \
-                             'geckodriver-v0.20.1-win64', 'geckodriver.exe'),
-          edge: File.join(__dir__, '..', 'bin', 'edge', \
-                          'microsoft_webdriver_win64', \
-                          'MicrosoftWebDriver.exe'),
-          ie: File.join(__dir__, '..', 'bin', 'internet_explorer', \
-                        'IEDriverServer_Win32_3.9.0', \
-                        'IEDriverServer.exe')
-        }
+     # Stores all relative paths for the repo's drivers
+     @@drivers = {
+      linux: {
+        chrome: File.join(__dir__, '..', 'bin', 'chrome', \
+                          'chromedriver_linux64', 'chromedriver'),
+        firefox: File.join(__dir__, '..', 'bin', 'firefox', \
+                           'geckodriver-v0.20.1-linux64', 'geckodriver')
+      },
+      macos: {
+        chrome: File.join(__dir__, '..', 'bin', 'chrome', \
+                          'chromedriver_mac64', 'chromedriver'),
+        firefox: File.join(__dir__, '..', 'bin', 'firefox', \
+                           'geckodriver-v0.20.1-macos', 'geckodriver'),
+        safari: File.join('/', 'usr', 'bin', 'safaridriver')
+      },
+      windows: {
+        chrome: File.join(__dir__, '..', 'bin', 'chrome', \
+                          'chromedriver_win32', 'chromedriver.exe'),
+        firefox: File.join(__dir__, '..', 'bin', 'firefox', \
+                           'geckodriver-v0.20.1-win64', 'geckodriver.exe'),
+        edge: File.join(__dir__, '..', 'bin', 'edge', \
+                        'microsoft_webdriver_win64', \
+                        'MicrosoftWebDriver.exe'),
+        ie: File.join(__dir__, '..', 'bin', 'internet_explorer', \
+                      'IEDriverServer_Win32_3.9.0', \
+                      'IEDriverServer.exe')
       }
-      # Make sure all of these files are executable
-      drivers.each do |_os, apps|
-        apps.each do |_app, path|
-          File.chmod(0o755, path) unless File.executable?(path)
-        end
-      end
-      # Set up Selenium logs
-      log_directory = File.join(__dir__, '..', 'log')
-      Dir.mkdir(log_directory) unless File.exist?(log_directory)
-      Selenium::WebDriver.logger.level = :debug
-      Selenium::WebDriver.logger.output = File.join(__dir__, '..', 'log', 'selenium.log')
-
-      # Firefox and Chrome have binaries for all platforms
-      Selenium::WebDriver::Firefox.driver_path = drivers[self.actual_os][:firefox]
-      Selenium::WebDriver::Chrome.driver_path = drivers[self.actual_os][:chrome]
-      # conditional for platform dependent browsers
-      if self.actual_os == :macos
-        Selenium::WebDriver::Safari.driver_path = drivers[:macos][:safari]
-      elsif self.actual_os == :windows
-        Selenium::WebDriver::Edge.driver_path = drivers[:windows][:edge]
-        Selenium::WebDriver::IE.driver_path = drivers[:windows][:ie]
-      end
-    end
-
+    }
     # Verifies if the browser and platform comination is valid
     def browser_valid?
-      !drivers[self.actual_os][browser].nil?
+      pp self.class.actual_os
+      pp @browser
+      !drivers[self.class.actual_os][@browser].nil?
     end
 
     # Utilize OS gem to better detect host OS
@@ -96,12 +75,38 @@ module Config
         raise UnsupportedOSError.new(OS.host_os, "Your operating system is not supported")
       end
     end
+
+    def initialize(browser)
+      # Make sure all of these files are executable
+      @@drivers.each do |_os, apps|
+        apps.each do |_app, path|
+          File.chmod(0o755, path) unless File.executable?(path)
+        end
+      end
+      # Set up Selenium logs
+      log_directory = File.join(__dir__, '..', 'log')
+      Dir.mkdir(log_directory) unless File.exist?(log_directory)
+      Selenium::WebDriver.logger.level = :debug
+      Selenium::WebDriver.logger.output = File.join(__dir__, '..', 'log', 'selenium.log')
+
+      # Firefox and Chrome have binaries for all platforms
+      Selenium::WebDriver::Firefox.driver_path = @@drivers[self.class.actual_os][:firefox]
+      Selenium::WebDriver::Chrome.driver_path = @@drivers[self.class.actual_os][:chrome]
+      # conditional for platform dependent browsers
+      if self.class.actual_os == :macos
+        Selenium::WebDriver::Safari.driver_path = @@drivers[:macos][:safari]
+      elsif self.class.actual_os == :windows
+        Selenium::WebDriver::Edge.driver_path = @@drivers[:windows][:edge]
+        Selenium::WebDriver::IE.driver_path = @@drivers[:windows][:ie]
+      end
+
+      @browser = browser
+    end
   end
 
   # Class for building a driver for the appropriate OS/browser version
   # and binding it to the requested application
   class App
-    attr_reader :browser
     attr_reader :app_url
 
     def initialize(configuration, url = 'google.com', protocol = 'https')
@@ -112,7 +117,7 @@ module Config
       end
 
       if configuration.browser_valid?
-        @browser = configuration.browser
+        Selenium::WebDriver.for configuration.browser
       else
         raise UnknownBrowserError
           .new(self.browser, 'Browser specified is not supported')
